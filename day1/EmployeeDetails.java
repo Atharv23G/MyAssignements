9a199e44cee284c29c9450456e2d0519be6e9904
Atharva Gurram <139481414+Atharv23G@users.noreply.github.com>
2025-12-06 11:17:19 -0500

    Add files via upload
    day1

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class EmployeeDetails 
+{
+
+	public static void main(String[] args) 
+	{
+		String name="Atharva Patil";
+		int age=25;
+		float sal=34500.02f;
+		char gender='M';
+		int pincode=56001;
+		boolean acceptterms=true;
+		
+		System.out.println("Employee Details:\n"+name+"\n"+age+"\n"+sal+"\n"+gender+"\n"+pincode+"\n"+acceptterms);
+		
+	}
+
+}
