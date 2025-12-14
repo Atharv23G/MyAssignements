5f3dafc39c1b33e1a9159105a1a2433fc31fe764
Atharva Gurram <139481414+Atharv23G@users.noreply.github.com>
2025-12-14 02:14:17 -0500

    Add files via upload

---
+++ week1/day1/Firefox1.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Firefox1 
+{
+
+		public static void main(String[] args) 
+		{		 
+			float browserVersion=100.2f;
+			String browserName="firefox";
+			boolean isVisible=true;
+			int releaseYear = 1998;
+			char browserLogo = 'f';
+			
+			System.out.println("browserDetails:\n"+browserVersion+"\n"+browserName+"\n"+isVisible+"\n"+releaseYear+"\n"+
+			browserLogo);
+		}
+
+
+
+}
